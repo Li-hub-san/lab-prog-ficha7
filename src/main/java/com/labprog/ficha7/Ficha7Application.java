cdb9f81b8f84e5f3fb696ffce152784016c6f2a6
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-03-11 18:21:57 +0000

    Initial commit - Spring framework.

---
+++ src/main/java/com/labprog/ficha7/Ficha7Application.java
@@ -0,0 +1,13 @@
+package com.labprog.ficha7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ficha7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ficha7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/labprog/ficha7/Ficha7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labprog.ficha7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ficha7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
